f5052be06f7931a5de8811fb6f8c088f59126051
Nathan-eng-2n <nathangeoo@gmail.com>
2024-06-28 23:06:50 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+// Adicione scripts JavaScript conforme necessário para a interação
+document.addEventListener('DOMContentLoaded', (event) => {
+    console.log('Página carregada completamente');
+});
